b98fd97b336264c920c5c9ed22eb9f88aee964c8
Juraganet <juraganet@gmail.com>
2017-08-18 13:18:55 +0700

    Update vpnsetup.sh

--- vpnsetup.sh
+++ vpnsetup.sh
@@ -325,7 +325,7 @@ topology subnet
 server 10.8.0.0 255.255.255.0
 ifconfig-pool-persist ipp.txt" > /etc/openvpn/server.conf
 echo 'push "route 10.8.0.0 255.255.255.0"' >> /etc/openvpn/server.conf
-echo 'push "route $IP 255.255.255.0"' >> /etc/openvpn/server.conf
+echo 'push "route '$IP' 255.255.255.0"' >> /etc/openvpn/server.conf
 #echo 'push "redirect-gateway def1 bypass-dhcp"' >> /etc/openvpn/server.conf
 	# DNS
 	case $DNS in
